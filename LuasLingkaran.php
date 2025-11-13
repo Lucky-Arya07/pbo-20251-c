c29a8b0392b7cef7b37752d1715321bbe002228f
Lucky-Arya07 <luckybagas681@gmail.com>
2025-11-13 13:59:10 +0700

    first commit

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -4,20 +4,12 @@ class LuasLingkaran {
 
     public const phi = 3.14;
     public int $jari;
-
-    public function tampil($nama = 'ban') {
-        $rumus LuasLingakaran::phi * ($this->jari * $this->jari);
-        echo "Lingkaran {$nama} hasilnya aadalah: {$rumus}";
-    }
-
-    public static function testing() {
-        echo "<br/>";
-        echo "ini dari static";
-    }
 }
+
 
 $lingkaran = new LuasLingkaran();
 $lingkaran->jari = 12;
-$lingkaran->tampil('roda'); //panggil method tampil
 
-LuasLingkara::testing(); //panggil static method
+
+$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+echo "hasilnya adalah:".$rumus;
